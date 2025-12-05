8c155fdea30608c68645440fe0d9dfb489842d78
Adriane Maciel <adrianemaciel.developer@gmail.com>
2025-12-05 17:18:33 -0300

    refactor server setup: organize imports, add middlewares, and establish MongoDB connection

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("API Trip Route Manager rodando!");
-});
-
 const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+// Importa as rotas de autenticação
+const authRoutes = require("./src/routes/authRoutes");
+
+// --- Middlewares ---
+app.use(cors());
+app.use(express.json());
+
+// --- Conexão com o Banco de Dados ---
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("MongoDB conectado com sucesso!"))
+  .catch((err) => console.error("Erro de conexão com MongoDB:", err));
+
+// --- Rotas ---
+app.use("/api/auth", authRoutes);
+
+// --- Inicialização do Servidor ---
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
