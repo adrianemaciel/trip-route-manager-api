2fc409bbef1dbbab39d1eb85c071c809cbbf4f6a
Adriane Maciel <adrianemaciel.developer@gmail.com>
2025-12-09 17:33:07 -0300

    refactor: clean up comments and improve code organization

--- server.js
+++ server.js
@@ -7,23 +7,18 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
-// Importa as rotas de autenticação
 const authRoutes = require("./src/routes/authRoutes");
 
-// --- Middlewares ---
 app.use(cors());
 app.use(express.json());
 
-// --- Conexão com o Banco de Dados ---
 mongoose
   .connect(MONGO_URI)
   .then(() => console.log("MongoDB conectado com sucesso!"))
   .catch((err) => console.error("Erro de conexão com MongoDB:", err));
 
-// --- Rotas ---
 app.use("/api/auth", authRoutes);
 
-// --- Inicialização do Servidor ---
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
